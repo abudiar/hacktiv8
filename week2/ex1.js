f0ba538f4cb6d59d63979238c3bfcf9573113134
abudiar <alanwbudiarto@gmail.com>
2020-01-27 14:35:42 +0700

    Add Week 2

---
+++ week2/ex1.js
@@ -0,0 +1,44 @@
+// # 1
+// VARIABLE mass IS 600kg
+// VARIABLE acceleration IS 2m/s^2
+// VARIABLE result IS 0N
+
+// result IS mass MULTIPLIED acceleration
+
+
+// # 2
+// VARIABLE year
+
+// IF year % 4 EQUALS 0 THEN 
+//     IF year % 100 EQUALS 0 THEN
+//         IF year % 400 EQUALS 0 THEN
+//             DISPLAY year is leap year
+//         ELSE
+//             DISPLAY year is not leap year
+//         ENDIF
+//     ELSE 
+//         DISPLAY year is leap year
+//     ENDIF
+// ELSE
+//     DISPLAY year is not leap year
+// ENDIF
+
+
+// # 3
+// VARIABLE clothesInMachine
+
+// DO 
+//     DISPLAY 'needs ' + (20 - clothesInMachine) + 'more clothes'
+// WHILE clothesInMachine LESS THAN 20
+
+
+// # 4
+// VARIABLE kukuSiswawi
+
+// FOR i = 0 to i = 40
+//     IF kukuSiswawi(i) EQUALS panjang THEN
+//         DISPLAY 'Siswa/Siswi Dihukum'
+//     ELSE
+//         DISPLAY 'Siswa/Siswi Dipuji'
+//     END IF
+// END FOR
